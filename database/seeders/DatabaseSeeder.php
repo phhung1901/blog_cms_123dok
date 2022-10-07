7390fb1811af6ba0fb0e6500d6273767bb824bf0
Nguyễn Gia Hào <giahao9899@gmail.com>
2022-10-07 15:43:34 +0700

    default admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if (config('app.env') == 'local') {
+            \App\Models\User::firstOrCreate([
+                'email' => 'admin@admin.com',
+            ], [
+                'name' => 'Administrator',
+                'password' => bcrypt('password'),
+            ]);
+        }
     }
 }
