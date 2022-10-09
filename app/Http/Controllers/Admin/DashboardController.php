69eec04004768214a8ef791c70296efc7521cd48
phhung1901 <thung18081901@gmail.com>
2022-10-09 23:27:48 +0700

    factory public

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+
+    public function index()
+    {
+        return view("admin.dashboard.index", [
+            "title" => "Dashboard"
+        ]);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
+    }
+}
