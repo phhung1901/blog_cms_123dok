484ca1f981a2f3328ceeb4ff797ca8d17d55ce48
phhung1901 <hung94464@nuce.edu.vn>
2022-10-17 17:38:25 +0700

    Fix BUG post,create and show POST

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -6,7 +6,6 @@
     </a>
 
     <div class="sidebar">
-
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="{{asset("/asset_admin/img/user2-160x160.jpg")}}" class="img-circle elevation-2" alt="User Image">
